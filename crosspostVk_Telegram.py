185024a077c6fbf6345da03c8b85cba02b9450df
Pavel Botsman <pavel-botsman@yandex.ru>
2015-08-25 22:45:48 +0300

    VK=>Telegram finished

--- crosspostVk_Telegram.py
+++ crosspostVk_Telegram.py
@@ -3,8 +3,9 @@ import json
 import time
 
 token = 'ololo'
+
 def getlongPollServer():
-"""делает запрос метода messages.getLongPollServer к vk API, в ответе возвращает кортеж из messages.getLongPollServer"""
+    """делает запрос метода messages.getLongPollServer к vk API, в ответе возвращает кортеж из messages.getLongPollServer"""
     params = urllib.parse.urlencode({'access_token':token})
     response = json.loads(urllib.request.urlopen('https://api.vk.com/method/messages.getLongPollServer?%s' % params).read().decode('UTF-8'))
     server = response['response']['server']
@@ -13,7 +14,7 @@ def getlongPollServer():
     return server, key, ts
 
 def getNewMessages(server, key, ts):
-    response = urllib.request.urlopen('http://%s?act=a_check&key=%s&ts=%s&wait=25&mode=0' % (server, key, ts)).read().decode('UTF-8')
+    response = urllib.request.urlopen('http://%s?act=a_check&key=%s&ts=%s&wait=25&mode=2' % (server, key, ts)).read().decode('UTF-8')
     return response
 
 def getUserInfo(id):
@@ -21,30 +22,37 @@ def getUserInfo(id):
     firstName = response['response'][0]['first_name']
     return firstName
 
-def getUserInfoByMessageId(messageId):
+"""def getUserInfoByMessageId(messageId):
     params = urllib.parse.urlencode({'message_ids':messageId, 'access_token': token})
     response = json.loads(urllib.request.urlopen('https://api.vk.com/method/messages.getById?%s' % params).read().decode('UTF-8'))
-    return response['response'][1]['uid']
+    return response['response'][1]['uid']"""
 
 def sendMessageTelegram(text):
     params = urllib.parse.urlencode({'chat_id':'-14476920','text':text})
-    response = urllib.request.urlopen('https://api.telegram.org/bot%s/sendMessage?%s' % ('137230011:AAFMRRRNr3ufNEjEYXTm77378-gUJVmmL6A', params))
+    response = urllib.request.urlopen('https://api.telegram.org/bot%s/sendMessage?%s' % ('lololo', params))
 
-NewMessages = json.loads(getNewMessages(getlongPollServer()[0], getlongPollServer()[1], getlongPollServer()[2]))
-server = getlongPollServer()[0]
-key = getlongPollServer()[1]
-ts = getlongPollServer()[2]
+def log(*arg):
+  log = open ('log.txt', 'a')
+  log.seek(0,2)
+  log.write(time.ctime() + ':')
+  log.write(str(arg) + '\n')
+
+longPollServer = getlongPollServer()
+server = longPollServer[0]
+key = longPollServer[1]
+ts = longPollServer[2]
+NewMessages = json.loads(getNewMessages(server, key, ts))
 while True:
     if 'failed' not in NewMessages:
-        ts = NewMessages['ts']
         for i in range(len(NewMessages['updates'])):
-            if NewMessages['updates'][i][0] == 4 and NewMessages['updates'][i][-2] == 'Дно':
-                print(NewMessages)
-                messageId = str(NewMessages['updates'][i][1])
-                message = NewMessages['updates'][i][-1]
-                sendMessageTelegram(getUserInfo(getUserInfoByMessageId(messageId)) + ': ' + message)
+            if NewMessages['updates'][i][0] == 4 and NewMessages['updates'][i][-3] == 'Дно':
+                log(NewMessages)
+                uid = NewMessages['updates'][i][-1]['from']
+                message = NewMessages['updates'][i][-2]
+                sendMessageTelegram(getUserInfo(uid) + ': ' + message)
         NewMessages = json.loads(getNewMessages(server, key, ts))
-    else:
-        NewMessages = getNewMessages(getlongPollServer()[0], getlongPollServer()[1], getlongPollServer()[2])
         ts = NewMessages['ts']
-        print(NewMessages, ts)
+    else:
+        longPollServer = getlongPollServer()
+        NewMessages = getNewMessages(longPollServer[0], longPollServer[1], longPollServer[2])
+        ts = NewMessages['ts']
